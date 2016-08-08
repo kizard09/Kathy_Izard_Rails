d61dfaccb9829ad4368e544c0cc384652e68846e
Kailey <kailey.izard@gmail.com>
2016-08-08 09:12:36 -0400

    First commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  before_filter :authenticate_user!, :categories, :brands
+
+  def categories
+  	@categories = Category.all
+  end
+
+  def brands
+  	@brands = Product.pluck(:brand).sort.uniq!
+  	if @brands == nil
+  		@brands = Product.pluck(:brand).sort
+  	end
+  end
+end
